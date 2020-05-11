d5fcc42a5994ca6f63df9a69c86a057b060627ce
nemotime <benjonant@msn.com>
2020-05-10 22:41:23 -0400

    added doubles

--- src/App.js
+++ src/App.js
@@ -24,15 +24,22 @@ e.g. When 4 total is rolled, give sips (4 sips)
 Dimuneur for placing mines to cancel a roll, count how many sips people have drank and grant them a "CAN" to mine people's rolls
 People who are mined have a % chance to hit the mine
 Parse for "next player" and "previous player" maybe RANDOM PLAYER?
+Program for giving sips to desired player, update player sips using prevState
 */
 
 class App extends Component {
   render() {
     const playerCount = this.state.players.filter((player) => player.name)
       .length;
+    const { players, currentPlayer, givingSips, givenSips } = this.state;
     return (
       <div className="App">
-        <h1>Three Man</h1>
+        <h1>
+          {this.state.threeMan !== null
+            ? this.state.players[this.state.threeMan].name + ' is '
+            : ''}
+          Three Man
+        </h1>
         <h2>
           Player order:{' '}
           {this.state.players.map((player, index) => (
@@ -42,7 +49,14 @@ class App extends Component {
             </span>
           ))}
         </h2>
-        <h2>{this.state.promptTextSum}</h2>
+        {this.state.playersSet ? (
+          <h1>
+            {this.state.players[this.state.currentPlayer].name}
+            's turn
+          </h1>
+        ) : (
+          ''
+        )}
         <div className="buttons">
           {this.state.playersSet ? (
             [1, 2, 3, 4, 5].map((number) => {
@@ -89,7 +103,37 @@ class App extends Component {
               </form>
             </div>
           )}
+          {this.state.playersSet ? (
+            <button className="endButton" onClick={this.setNextPlayer}>
+              End Turn
+            </button>
+          ) : (
+            ''
+          )}
         </div>
+        <h1>
+          {this.state.playersSet && this.state.rollSum
+            ? this.state.players[this.state.currentPlayer].name + "'s roll:"
+            : ''}
+        </h1>
+        <h2>{this.state.promptTextSum}</h2>
+        {givingSips ? (
+          <h2>Who do you want to give {this.state.rolls[0] * 2} sips to?</h2>
+        ) : (
+          ''
+        )}
+        {givingSips
+          ? players.map((player, index) => {
+              if (player === players[currentPlayer]) return '';
+              return (
+                <button
+                  key={index}
+                  onClick={() => this.displayGivenSips(index)}
+                ></button>
+              );
+            })
+          : ''}
+        {givenSips}
         {this.state.rolls.map((roll, index) => (
           <DiceImage roll={roll} key={index} />
         ))}
@@ -102,9 +146,21 @@ class App extends Component {
     rollSum: null,
     promptTextSum: '',
     playersSet: false,
-    players: [{ name: '' }],
+    players: [{ name: '', sips: 0 }],
     currentPlayer: 0,
+    threeMan: null,
+    givingSips: false,
+    givenSips: '',
+    sipsCirculating: 0,
   };
+
+  whoRolled() {
+    if (this.state.currentPlayer - 1 === -1) {
+      return this.state.players.filter((player) => player.name).length - 1;
+    } else {
+      return this.state.currentPlayer - 1;
+    }
+  }
 
   handlePlayerNameChange = (idx) => (evt) => {
     const newPlayers = this.state.players.map((player, sidx) => {
@@ -139,11 +195,13 @@ class App extends Component {
       rolls[i] = Math.floor(Math.random() * 6) + 1;
       rollSum += rolls[i];
     }
+    let givenSips = '';
     this.setState(
       {
         numberOfDice,
         rolls,
         rollSum,
+        givenSips,
       },
       () => {
         this.getResult();
@@ -151,6 +209,8 @@ class App extends Component {
       },
     );
   };
+  // endTurn() {
+  // }
   resolveRules() {
     return;
   }
@@ -163,6 +223,7 @@ class App extends Component {
         break;
       case 3:
         text = 'You are Three Man';
+        this.setThreeMan(this.state.currentPlayer);
         break;
       case 7:
         text = 'Person before you drinks';
@@ -175,15 +236,105 @@ class App extends Component {
         this.makeRule();
         break;
       default:
-        text = 'Nothing happens';
+        text = '';
         break;
+    }
+    text = this.checkEachDice(text);
+    if (text.length === 0) {
+      text = 'Nothing happens';
     }
     console.log(text);
     return this.setState({
       promptTextSum: text,
     });
   }
+
+  checkEachDice(text) {
+    const { threeMan, players, rolls } = this.state;
+    let addedText = text;
+
+    addedText += rolls.map((roll, index) => {
+      switch (roll) {
+        case 3:
+          if (!threeMan) return '';
+          return '\nThree man ' + players[threeMan].name + ' drinks';
+        default:
+          return '';
+      }
+    });
+    return this.checkPair(addedText, rolls);
+  }
+  checkPair(text, rolls) {
+    if (rolls.length > 1) {
+      if (rolls[0] === rolls[1] && rolls[0] !== 1) {
+        this.giveSomeoneSips(rolls[0] * 2);
+      }
+    }
+    return text;
+  }
+  setThreeMan(player) {
+    this.setState({
+      threeMan: player,
+    });
+  }
+
+  giveSelfSip(sips) {}
+
+  giveSomeoneSips(sips) {
+    const givingSips = true;
+    const sipsCirculating = sips;
+    this.setState({
+      givingSips,
+      sipsCirculating,
+    });
+
+    // const { players, currentPlayer } = this.state;
+    // players.map((player, index) => {
+    //   if (player === players[currentPlayer]) return '';
+    //   return (
+    //     <button
+    //       key={index}
+    //       onClick={() => this.displayGivenSips(index)}
+    //     ></button>
+    //   );
+    // });
+  } //in this one, let the player choose who
+
+  displayGivenSips(playerNum) {
+    let { players, currentPlayer, sipsCirculating } = this.state;
+    const givenSips =
+      players[currentPlayer].name +
+      ' gave ' +
+      sipsCirculating +
+      ' sips to ' +
+      players[playerNum].name +
+      '.';
+    const givingSips = false;
+    this.setState({
+      givenSips,
+      givingSips,
+    });
+  }
   makeRule() {}
+
+  setNextPlayer = () => {
+    let currentPlayer = 0;
+    let rolls = [];
+    let rollSum = 0;
+    let promptTextSum = '';
+    if (
+      this.state.currentPlayer !==
+      this.state.players.filter((player) => player.name).length - 1
+    ) {
+      currentPlayer = this.state.currentPlayer + 1;
+    }
+    this.setState({
+      currentPlayer,
+      rolls,
+      rollSum,
+      promptTextSum,
+    });
+  };
 }
 
 const DiceImage = ({ roll }) => {
