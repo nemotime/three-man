38873518c0a44d730723f55cb49858eb9dca2ef3
nemotime <benjonant@msn.com>
2020-05-23 03:09:38 -0400

    Updates

---
+++ precache-manifest.53f2bc095d9a64968f35b31f6c33b999.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dedefdae913033b5bb4d1232992e6e5",
+    "url": "/three-man/index.html"
+  },
+  {
+    "revision": "5e05d9c5823a4f2c8a0c",
+    "url": "/three-man/static/css/main.ef2108bb.chunk.css"
+  },
+  {
+    "revision": "e82a4ff6ec11a4d1dad6",
+    "url": "/three-man/static/js/2.36399c37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/three-man/static/js/2.36399c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e05d9c5823a4f2c8a0c",
+    "url": "/three-man/static/js/main.246f4ab7.chunk.js"
+  },
+  {
+    "revision": "7a44a2d7329e7b0d03a3",
+    "url": "/three-man/static/js/runtime-main.94cc3872.js"
+  },
+  {
+    "revision": "eafad47540d592fad2cc8e548fa2704a",
+    "url": "/three-man/static/media/1-2.eafad475.wav"
+  },
+  {
+    "revision": "94728d64747580afbf0d5134d55babc1",
+    "url": "/three-man/static/media/3-3.94728d64.wav"
+  },
+  {
+    "revision": "d3c08da348220949163e99c971f58120",
+    "url": "/three-man/static/media/L50.d3c08da3.png"
+  },
+  {
+    "revision": "6c056c0d80ec479b3eb9e9604b965aaf",
+    "url": "/three-man/static/media/dice-roll.6c056c0d.mp3"
+  },
+  {
+    "revision": "1826aec4a09a1b8f02bdce91c1d65621",
+    "url": "/three-man/static/media/dice.1826aec4.gif"
+  },
+  {
+    "revision": "0c8e342d658db46a3d00ab4650bc9373",
+    "url": "/three-man/static/media/end-turn.0c8e342d.mp3"
+  },
+  {
+    "revision": "9abdc58316fd516e4b6af18048c742d6",
+    "url": "/three-man/static/media/hit-mine.9abdc583.mp3"
+  },
+  {
+    "revision": "195d0c5e2ed511b1a7d80bcf6c2c9c91",
+    "url": "/three-man/static/media/hitmarker.195d0c5e.mp3"
+  },
+  {
+    "revision": "d65eaa73d643b7fdadd79cbc1bb1533e",
+    "url": "/three-man/static/media/join.d65eaa73.mp3"
+  },
+  {
+    "revision": "c04995caceabaff78cb0f5fbdf4cecfe",
+    "url": "/three-man/static/media/leave.c04995ca.mp3"
+  },
+  {
+    "revision": "0b5f7b5ac7189895f4814cded3f161d7",
+    "url": "/three-man/static/media/mel.0b5f7b5a.mp3"
+  },
+  {
+    "revision": "17b40f7349d6a17124e535e14c8ca820",
+    "url": "/three-man/static/media/meldiploma.17b40f73.jpg"
+  },
+  {
+    "revision": "06209ad1110edd2b38c2664bfb09da6f",
+    "url": "/three-man/static/media/mine.06209ad1.mp3"
+  },
+  {
+    "revision": "7111f7c41cd524564be5d67d5498318f",
+    "url": "/three-man/static/media/spank.7111f7c4.mp3"
+  }
+]);
